2f4967b9a87b97fc37f2d364978ca9969e55389e
akgithub755 <akgithub755@gmail.com>
2024-09-28 18:13:30 +0530

    add another view

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
@@ -10,6 +10,12 @@ def home():
 @app.route('/user/<name>')
 def user(name):
     return f"hi {name}"
+
+
+@app.route('/user-agent')
+def index():
+    user_agent = request.headers.get('User-Agent')
+    return f"your broswer is {user_agent}"
 
 
 if __name__ == '__main__':
